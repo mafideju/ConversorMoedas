0a288cd78a7e393e035e9f66b2195f55855eb686
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-12-16 11:06:01 -0200

    Axios Calls

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+const axios = require('axios');
 
+// const taxaConversao = (from, to) => {
+//   return axios.get('http://data.fixer.io/api/latest?access_key=85ec9ef5f2a0314746001157d4bca4e8').then(res => {
+//     const euro = 1 / res.data.rates[from];
+//     const rate = euro * res.data.rates[to];
+//     return rate;
+//   });
+// };
 
-// http://data.fixer.io/api/latest?access_key=85ec9ef5f2a0314746001157d4bca4e8
+const taxaConversao = async (from, to) => {
+  const res = await axios.get('http://data.fixer.io/api/latest?access_key=85ec9ef5f2a0314746001157d4bca4e8');
+  const euro = 1 / res.data.rates[from];
+  const rate = euro * res.data.rates[to];
+
+  return rate;
+};
+
+const buscaPaises = async (codigoPais) => {
+  const res = await axios.get(`https://restcountries.eu/rest/v2/currency/${codigoPais}`);
+  const name = res.data.map(pais => pais.name);
+  return name;
+}
+
+taxaConversao('USD', 'BRL').then(rate => {
+  console.log(`Um dólar custa atualmente R$${rate.toFixed(2)}.`)
+});
+
+buscaPaises('BRL').then(nome => {
+  console.log(nome)
+})
