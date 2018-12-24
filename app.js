546bfcb1f8336fe441a28c544b42c3c4de21fe68
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-12-24 00:56:14 -0200

    Data Displays

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ const axios = require('axios');
 //   });
 // };
 const periodo = '2010-01-01';
-
+// latest para periodo mais recente
 const taxaConversao = async (from, to) => {
   try {
     const res = await axios.get(`http://data.fixer.io/api/${periodo}?access_key=85ec9ef5f2a0314746001157d4bca4e8`);
     const euro = 1 / res.data.rates[from];
+    console.log('rates by alpha3', res.data.rates)
+    console.log('origem', from, 'destino', to)
     const rate = euro * res.data.rates[to];
 
     if (isNaN(rate)) {
       throw new Error();
     }
 
+    console.log('Rate', rate, to, from);
     return rate;
   } catch (e) {
     throw new Error(`${e} Estamos com problema para converter ${from} para ${to}.`);
@@ -28,9 +31,11 @@ const taxaConversao = async (from, to) => {
 const buscaPaises = async (codigoPais) => {
   try {
     const res = await axios.get(`https://restcountries.eu/rest/v2/currency/${codigoPais}`);
+    console.log("Codigo Pais", codigoPais)
     const name = res.data.map(pais => pais.name);
+    console.log('Nome do Pais', name)
 
-    return name
+    return name;
   } catch (e) {
     throw new Error(`${e} Servidor com problemas para encontrar o país relativo ao código ${codigoPais}.`);
   }
@@ -43,7 +48,7 @@ const conversorMoedas = async (from, to, quantia) => {
     const paisOrigem = await buscaPaises(from);
     const paisDestino = await buscaPaises(to);
 
-    return `Em ${periodo}, a quantia de ${quantia} ${from}, moeda do ${paisOrigem.join(', ')}, estão valendo ${quantiaConvertida} ${to}, que é a moeda aceita nos seguintes países: ${paisDestino.join(', ')}.`;
+    return `Em ${periodo}, a quantia de ${quantia} ${from}, moeda aceita no ${paisOrigem.join(', ')}, vale ${quantiaConvertida} ${to}, que é a moeda aceita no ${paisDestino.join(', ')}.`;
   } catch (e) {
     throw new Error(`${e} - Problemas na conversão final.`);
   }
